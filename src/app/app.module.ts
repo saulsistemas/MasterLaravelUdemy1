4d791d89a943ff68dd573e11ab5b62ae9e6dd3cb
saul <saul@.hotmail.com>
2022-03-22 17:43:53 -0500

    property binding aplicando estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InterpolationComponent
+    InterpolationComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule
