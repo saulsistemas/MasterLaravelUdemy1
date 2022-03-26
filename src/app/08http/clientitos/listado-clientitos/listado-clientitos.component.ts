6adca0f0702385509d3c4e5cc57284bc3bd8c82d
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-03-26 15:56:14 -0500

    http client con api rest

--- src/app/08http/clientitos/listado-clientitos/listado-clientitos.component.ts
+++ src/app/08http/clientitos/listado-clientitos/listado-clientitos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientitosService } from '../../servicios/clientitos.service';
 
 @Component({
   selector: 'app-listado-clientitos',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoClientitosComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private clientesService:ClientitosService) { }
+  clientitos :any;
+  id_cliente:number = 2;
+  usuario :any;
   ngOnInit(): void {
+    
+    this.cargarClientitos();
+    this.cargarUnCliente();
+    
+    //console.log(this.clientesService.getClientes().subscribe());
+    
+    //this.clientesService.getClientes()
+    //                    .subscribe((response:any)=>{
+    //                      console.log(response);
+    //                      
+    //                    },(error:any)=>{
+    //                      console.log(error);
+    //                      
+    //                    })
+  }
+  cargarClientitos(){
+    this.clientesService.getAllUser().subscribe((result:any) =>{
+      console.log(result.data);
+      this.clientitos = result.data;
+      
+    },error =>{
+      console.log(error);
+      
+    });
   }
 
+  cargarUnCliente(){
+    this.clientesService.getUser(this.id_cliente).subscribe((result:any) =>{
+      console.log(result.data);
+      this.usuario = result.data;
+      
+    },error =>{
+      console.log(error);
+      
+    });
+  }
 }
--- src/app/08http/servicios/clientitos.service.ts
+++ src/app/08http/servicios/clientitos.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientitosService {
+  public url:string ='https://reqres.in/';
+  constructor(private _http:HttpClient){
 
-  constructor() { }
+  }
+  getUser(userId){
+    return this._http.get(this.url+'api/users/'+userId)
+    
+  }
+  getAllUser(){
+    return this._http.get(this.url+'api/users?page=2')
+  }
+
+  //endpointClientes: string = 'http://localhost:3000/';
+  //constructor(private http:HttpClient) { }
+  //getClientes(){
+  //  return this.http.get(this.endpointClientes).pipe(map((data:any)=>{return data}));
+  //  
+  //  //return this.http.get(this.endpointClientes)
+  //  //                .pipe(
+  //  //                  map((data:any)=>{
+  //  //                    return data;
+  //  //                  })
+  //  //                )
+  //}
 }
